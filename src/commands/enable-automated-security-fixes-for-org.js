0f3c4e6173bc58976211f44390ed327eeed6b20a
Aidan Feldman <aidan.feldman@gsa.gov>
2019-09-03 01:36:40 -0400

    format github security scripts using Prettier

--- src/commands/enable-automated-security-fixes-for-org.js
+++ src/commands/enable-automated-security-fixes-for-org.js
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
 
-require('dotenv').config()
-const pReduce = require('./lib/p-reduce');
-const delay = require('./lib/delay');
-const Octokit = require('@octokit/rest')
+require("dotenv").config();
+const pReduce = require("./lib/p-reduce");
+const delay = require("./lib/delay");
+const Octokit = require("@octokit/rest");
 const octokit = new Octokit({
   auth: process.env.GH_AUTH_TOKEN,
-  previews: ['london-preview']
-})
+  previews: ["london-preview"]
+});
 
-const [, , ...args] = process.argv
-const owner = args
-const options = octokit.repos.listForOrg.endpoint.merge({org: owner, type: 'all'})
+const [, , ...args] = process.argv;
+const owner = args;
+const options = octokit.repos.listForOrg.endpoint.merge({
+  org: owner,
+  type: "all"
+});
 
 octokit
   .paginate(options)
   .then(repositories =>
-    pReduce(repositories, (repository) => {
+    pReduce(repositories, repository => {
       if (repository.archived) {
         return Promise.resolve();
       }
-      const repo = repository.name
+      const repo = repository.name;
 
       return octokit.repos
         .enableAutomatedSecurityFixes({
@@ -29,9 +32,9 @@ octokit
         })
         .then(response => {
           if (response && response.status === 204) {
-            console.log(`Success for ${owner}/${repo}`)
+            console.log(`Success for ${owner}/${repo}`);
           } else {
-            console.log(`Failed for ${owner}/${repo}`)
+            console.log(`Failed for ${owner}/${repo}`);
           }
           return delay(500);
         })
@@ -39,13 +42,13 @@ octokit
           console.error(`Failed for ${owner}/${repo}
 ${error.message}
 ${error.documentation_url}
-`)
-        })
+`);
+        });
     })
   )
   .catch(error => {
     console.error(`Getting repositories for organization ${owner} failed.
   ${error.message} (${error.status})
   ${error.documentation_url}
-`)
-  })
+`);
+  });
