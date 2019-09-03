d3a289ca224f0a2ee075850dea50fc22bf8931df
Aidan Feldman <aidan.feldman@gsa.gov>
2019-09-03 01:59:11 -0400

    integrate sub-command for enabling security alerts

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const moment = require("moment");
 const { archiveStaleRepos } = require("./src/commands/archive");
+const {
+  enableSecurityAlerts
+} = require("./src/commands/enable-security-alerts-for-org");
 
 require("yargs")
   .scriptName("ghad")
   .usage("$0 <cmd> [options]")
+  .option("apply", {
+    type: "boolean",
+    describe: "Perform the action; without this, will only be a dry run"
+  })
   .command(
     "archive",
     "Archives inactive repositories.",
     yargs => {
       yargs.options({
-        apply: {
-          type: "boolean",
-          describe:
-            "Archive the repositories; without this, will only be a dry run"
-        },
         cutoff: {
           type: "number",
           default: 90,
@@ -32,6 +34,19 @@ require("yargs")
         org: argv.org
       };
       archiveStaleRepos(cutoff, opts);
+    }
+  )
+  .command(
+    "enable-security-alerts <org>",
+    "Enables security alerts.",
+    yargs => {
+      yargs.positional("org", {
+        describe:
+          "Enable the alerts for repositories in this GitHub user/organization"
+      });
+    },
+    argv => {
+      enableSecurityAlerts(argv.org, argv.apply);
     }
   )
   .strict()
